519d0f9aa19ec3e63b6079a4bf4e830a9013cef5
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2019-12-08 17:03:32 -0800

    server public and database setup

--- gif.sh
+++ gif.sh
@@ -10,13 +10,12 @@ TEMP="./temp"
 URL=$1
 TIME=$2
 LEN=$3
-RES=$4
 
 TIME=`date +'%s'`
 
-youtube-dl -q  -o "$TEMP/$TIME.mp4" -f "bestvideo[height<=360]" $URL
+youtube-dl -o "$TEMP/$TIME.mp4" -f "bestvideo[height<=360]" $URL
 
-ffmpeg -loglevel quiet -ss $TIME -i "$TEMP/$TIME.mp4" -t $LEN -vf "fps=20,scale=$RES" $DIR/$TIME.gif
+ffmpeg -ss $TIME -i "$TEMP/$TIME.mp4" -t $LEN -vf "fps=20" $DIR/$TIME.gif
 
 rm "$TEMP/$TIME.mp4"
 
